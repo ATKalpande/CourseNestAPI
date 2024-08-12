d6c106ca6ed13c990fb6c5d9abeaba4f51f5e4e3
Akash T KALPANDE <88917542+ATKalpande@users.noreply.github.com>
2024-08-12 17:26:40 +0530

    Add project files.

---
+++ Models/DTOs/CourseDTO.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CourseNest.Models.DTOs;
+public class CourseDTO
+{
+    public int Id { get; set; }
+
+    [Required]
+    [MaxLength(40)]
+    public string? CourseName { get; set; }
+
+    [Required]
+    [MaxLength(40)]
+    public string? InstructorName { get; set; }
+    [Required]
+    public double CourseFee { get; set; }
+    public string? Image { get; set; }
+    [Required]
+    public int CategoryId { get; set; }
+    [NotMapped]
+    public IFormFile? ImageFile { get; set; }
+    public IEnumerable<SelectListItem>? CategoryList { get; set; }
+}
